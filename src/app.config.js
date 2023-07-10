382de1bb81fe1fe2179272b71d0fe7ed44207e4b
Harsh Wandhare <81346147+CodeWithHarsh@users.noreply.github.com>
2023-07-10 11:58:06 +0530

    Add files via upload

---
+++ src/app.config.js
@@ -0,0 +1,21 @@
+const firebaseConfig = {
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXXXXXXXXXX",
+    databaseURL: "XXXXXXXXXXXXXXXXXXXXXX",
+    projectId: "XXXXXXXXXXXXXXX",
+    storageBucket: "XXXXXXXXXXXXXXXXXX",
+    messagingSenderId: "XXXXXXXXXXXXXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXXXXXXXX",
+    measurementId: "XXXXXXXXXXXXXXXXXXXXXXX"
+  };
+  
+  
+  const cometChat ={
+    APP_ID: 'XXXXXXXXXXXXXXXXX',
+    AUTH_KEY: 'XXXXXXXXXXXXXXXXXXXXX',
+    REST_KEY: 'xxxxxxxxxxxxxxxxxxxxx',
+    APP_REGION: 'xx',
+  
+  }
+  
+  export { firebaseConfig, cometChat }
